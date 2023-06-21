59934f58e17e41fc1602faa461fe426158ec2959
Moises Gonzalez <moises.gonzalez@evbox.com>
2023-06-21 02:17:28 +0200

    feat: implements initial census inference API

--- main.py
+++ main.py
@@ -1,1 +1,122 @@
-# Put the code for your API here.
+"""
+Script to create an API
+Author: Moises Gonzalez
+Date: 20/Jun/2023
+"""
+
+import pandas as pd
+import pickle
+from pathlib import Path
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from ml.data import process_data
+
+MODEL_PATH = Path(__name__).resolve().parent / "model"
+ENCODER_FILE = MODEL_PATH / "encoder.pkl"
+LB_FILE = MODEL_PATH / "label_binarizer.pkl"
+MODEL_FILE = MODEL_PATH / "trained_model.pkl"
+
+
+class Census(BaseModel):
+    """
+    Census data for user input
+    """
+    age: int
+    workclass: str
+    fnlgt: int
+    education: str
+    education_num: int
+    marital_status: str
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: int
+    capital_loss: int
+    hours_per_week: int
+    native_country: str
+
+    # TODO add valid values for each data
+
+    class Config:
+        schema_extra = {"example": {"age": 35,
+                                    "workclass": "Private",
+                                    "fnlgt": 7777,
+                                    "education": "Some-college",
+                                    "education_num": 10,
+                                    "marital_status": "str",
+                                    "occupation": "Armed-Forces",
+                                    "relationship": "Wife",
+                                    "race": "Other",
+                                    "sex": "Male",
+                                    "capital_gain": 0,
+                                    "capital_loss": 0,
+                                    "hours_per_week": 40,
+                                    "native_country": "Holand-Netherlands"}
+                        }
+
+
+app = FastAPI(
+    title="Census Inference API",
+    description="An API for the Census model",
+    version="1.0.0",
+)
+
+
+@app.get("/")
+def greet():
+    """
+    Welcome message
+    """
+    return {"message": "Welcome!"}
+
+
+@app.on_event("startup")
+async def load_model():
+    """
+    Event handler that verify that the model files exists
+    """
+    assert MODEL_FILE.exists()
+    assert ENCODER_FILE.exists()
+    assert LB_FILE.exists()
+
+
+@app.post("/predictions/")
+async def predict(data: Census):
+    """
+
+    """
+
+    input_data = {"workclass": data.age,
+                  "education": data.education,
+                  "marital_status": data.marital_status,
+                  "occupation": data.occupation,
+                  "relationship": data.relationship,
+                  "race": data.race,
+                  "sex": data.sex,
+                  "native_country": data.native_country}
+
+    input_dataframe = pd.DataFrame(input_data, index=[0])
+
+    features = ["workclass", "education", "marital_status", "occupation",
+                "relationship", "race", "sex", "native_country"]
+
+    model = pickle.load(open(MODEL_FILE, "rb"))
+    encoder = pickle.load(open(ENCODER_FILE, "rb"))
+    lb = pickle.load(open(LB_FILE, "rb"))
+
+    X, _y, _encoder, _lb = process_data(X=input_dataframe,
+                                        categorical_features=features,
+                                        training=False,
+                                        encoder=encoder,
+                                        lb=lb)
+
+    prediction = model.predict(X)
+
+    pred = ">50k" if prediction[0] else "<=50k"
+
+    input_dataframe["prediction"] = "<=50k"
+    if prediction[0] > 0.5:
+        input_dataframe["prediction"] = ">50k"
+
+    return {"prediction": pred}
